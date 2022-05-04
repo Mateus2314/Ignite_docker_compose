671465b1df6ac848ff93ebffd161502b39acdcb7
Mateus2314 <mateusfuntecezar10@hotmail.com>
2022-05-04 16:08:22 -0300

    Add UpdateAvatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
+import multer from "multer";
+import { UpdateUserAvatarController } from "../modules/accounts/updateUserAvatar/UpdateUserAvatarController";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
-
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer( uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
-
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle );
+
+usersRoutes.patch("/avatar", ensureAuthenticated , uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 
 export { usersRoutes };
